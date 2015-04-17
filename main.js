264e6750330fcea94c751e386f1135d088dc417a
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-04-17 18:23:35 +0100

    added change to pin 7

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ var board = new five.Board({
  
 board.on('ready', function() {
  	console.log("board is ready!")
-  // Create an Led on pin 8 (GPI14) on P1 and strobe it on/off
+  // Create an Led P1 and strobe it on/off
   // Optionally set the speed; defaults to 100ms
-  (new five.Led('P1-8')).strobe();
+  (new five.Led('P1-7')).strobe();
  
 });

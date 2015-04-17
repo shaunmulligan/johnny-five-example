01d9f2e81b3088dadffe4041d1c6e7e23ccf4040
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-04-17 17:45:03 +0100

    add kmod

--- deps.sh
+++ deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install module-init-tools
+apt-get update && apt-get install -y module-init-tools
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 ./build

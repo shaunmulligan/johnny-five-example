dd2cb4a77cf5344ee1a6d003fd936c1871760753
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-04-17 16:03:14 +0100

    comment out wiringPi for now

--- deps.sh
+++ deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git clone git://git.drogon.net/wiringPi
-cd wiringPi
-./build
+# git clone git://git.drogon.net/wiringPi
+# cd wiringPi
+# ./build
 
 npm install --ignore-scripts raspi-i2c
